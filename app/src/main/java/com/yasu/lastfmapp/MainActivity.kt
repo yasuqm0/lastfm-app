eb8909cd94ac4c270ea3f1c261529f5ca2ead222
vdelgado <yasuhigkx@gmail.com>
2026-06-01 19:32:13 -0500

    Add MainScreen with Scaffold and TopAppBar

--- app/src/main/java/com/yasu/lastfmapp/MainActivity.kt
+++ app/src/main/java/com/yasu/lastfmapp/MainActivity.kt
@@ -5,43 +5,21 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.yasu.lastfmapp.ui.screens.MainScreen
 import com.yasu.lastfmapp.ui.theme.LastFMAppTheme
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreate(savedInstance: Bundle?) {
+        super.onCreate(savedInstance)
         enableEdgeToEdge()
         setContent {
-            LastFMAppTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+            LastFMAppTheme() {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    MainScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    LastFMAppTheme {
-        Greeting("Android")
     }
 }
